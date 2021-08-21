1d70a4affc9947e174f4432f2bd0f5b70ba6d6f9
toshi01kudo <72183025+toshi01kudo@users.noreply.github.com>
2021-08-21 09:10:17 +0900

    Add chara-memo to solve #1

--- Youkoya-Charactor-Utilizer.user.js
+++ Youkoya-Charactor-Utilizer.user.js
@@ -1,12 +1,14 @@
 // ==UserScript==
 // @name         Youkoya Charactor Utilizer
 // @namespace    Youkoya Charactor Utilizer
-// @version      0.1
+// @version      0.2
 // @description  Tools to utilize the charactor sheet page of Youkoya.
 // @author       Toshihito Kudo
 // @include      https://charasheet.vampire-blood.net/*
 // @require      https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js
 // ==/UserScript==
+// 0.1 initial release
+// 0.2 Add charactor memo
 
 (function() {
     'use strict';
@@ -22,17 +24,43 @@
         $('#ExtractChara').on('click', function(){
             // パラメータ取得
             //const ShuzokuID = $("#V_shuzoku_id").val();
-            //const ShuzokuNAME = $("#shuzoku_name").val();
-            //const Henge = $("#V_NC1").val();
-            //const Kemono = $("#V_NC2").val();
-            //const Otona = $("#V_NC3").val();
-            //const Kodomo = $("#V_NC4").val();
+            const ShuzokuNAME = $("#shuzoku_name").val();
+            const Henge = $("#V_NC1").val();
+            const Kemono = $("#V_NC2").val();
+            const Otona = $("#V_NC3").val();
+            const Kodomo = $("#V_NC4").val();
             const Hushigi = $("#NP5").val();
             const Omoi = $("#NP6").val();
             const PC_NAME = $("#pc_name").val();
+            const PC_AGE = $("#age").val();
+            // キャラクターメモ作成
+            let MEMO = "■パーソナルデータ・経歴■\\n";
+            MEMO += "名前：" + PC_NAME + "\\n";
+            MEMO += "正体：" + ShuzokuNAME + "\\n";
+            MEMO += "年齢：" + PC_AGE + "\\n\\n■能力と弱点■\\n";
+            // 特技リスト取得
+            for (var r = 0; r<$("#Table_power tr th input").length; r++) {
+                MEMO += "[" + $("#Table_power tr th input").slice(r).val() + "]";
+                for (var i = 0; i<3; i++) {
+                    if (i==0) {
+                        MEMO += $("#Table_power tr td input").slice(r*4+i).val();
+                    } else if (i==1) {
+                        MEMO += " (" + $("#Table_power tr td input").slice(r*4+i).val() + ") :";
+                    } else if (i==2) {
+                        MEMO += $("#Table_power tr td input").slice(r*4+i).val() +"\\n";
+                    }
+                }
+            }
+            MEMO += "\\n■能力値■\\n";
+            MEMO += "へんげ： (" + Henge + ") = 不思議な力、変化たちのこと\\n";
+            MEMO += "けもの： (" + Kemono + ") = 走る、感じる、隠れる\\n";
+            MEMO += "おとな： (" + Otona + ") = 機械を使う、知識、気配り\\n";
+            MEMO += "こども： (" + Kodomo + ") = 遊ぶ、甘える、守ってもらう";
+            console.log(MEMO)
             // URL作成
             let CharaURL = '{"kind":"character","data":{"name":"';
             CharaURL += PC_NAME;
+            CharaURL += '","memo":"' + MEMO;
             CharaURL += '","externalUrl":"' + location.href;
             CharaURL += '","status":[{"label":"夢"},'
             CharaURL += '{"label":"ふしぎ","value":' + Hushigi + '},'
